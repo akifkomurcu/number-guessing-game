0592b0f7e6ab0ea990b39183c56a8f4d16db2b3d
akifkomurcu <46891831+akifzsche@users.noreply.github.com>
2021-07-21 20:43:23 +0300

    Add files via upload

---
+++ oyun.cpp
@@ -0,0 +1,88 @@
+//oyunda sayý tahmin ediyoruz ama bu diðerlerinden biraz farklý. Ýlk sayýyý tahmin etmeye çalýþtýðýnýzda 5 sayý arasýndan bilmeye çalýþýyorsunuz. Fakat sayýyý her bilemediðinizde
+//bu sayi 0-5 arasi degil 0-6 gibi artarak devam ediyor ve sayý tahmini zorlaþýyor. eðer 5 kez bilemezseniz oyun biter 3 kez bilirseniz oyunu kazanirsiniz. Her bildiginizde artan aralik azalir
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+int ras,rasgele1, i, seed;
+int bilemedin=0;
+int bildin=0;
+int sayigir;
+
+
+int main()
+{
+	cout<<"oyunda sayi tahmin ediyoruz ama bu digerlerinden biraz farkli. Ilk sayiyi tahmin etmeye calistiginizda 5 sayi arasindan bilmeye calisiyorsunuz. Fakat sayiyi her bilemediginizde bu sayi 0-5 arasi degil 0-6 gibi artarak devam ediyor ve sayi tahmini zorlasiyor. Eger 5 kez bilemezseniz kaybedersiniz, 3 kez bilirseniz kazanirsiniz. Her bildiginizde artan aralik azalir"<<endl;
+	seed = time(NULL);
+    srand(seed);
+	basadon:
+  	int sayilar[5+bilemedin];
+
+    rasgele1 = rand() % (5+bilemedin);
+    ras=rasgele1+1;
+    sayilar[ras] = 1;
+    cout<<endl;
+	cout<<"sayi girin "<<0<<"-"<<5+bilemedin<<endl;
+    cin >> sayigir;
+
+    if (sayilar[sayigir] == 1)
+    { 
+	cout << "kazandin" << endl;
+	bildin+=1;
+	bilemedin-=1;
+	goto basadon;
+	if(bildin==3)
+        	goto bitis;
+	 cout << "sayi : " << ras << "'de sakliydi" << endl;
+        goto bitis;
+}
+    else
+    {
+    	 cout << "sayi : " << ras << "'de sakliydi" << endl;
+    	sayilar[sayigir]=0;
+     sayilar[ras]=0;
+       	 bilemedin+=1;
+        	cout << "bilemedin : "<<bilemedin<<endl;
+        	
+        	if(bilemedin==5)
+        	goto bitis;
+        	else
+				goto basadon;
+	}	
+
+    for (int i = 1; i < 5+bilemedin; i++)
+    {
+        if (sayilar[sayigir] == sayilar[i])
+        {
+        	cout << "kazandin" << endl;
+        	bildin+=1;
+        	bilemedin-=1;
+        	goto basadon;
+        	if(bildin==3)
+        	goto bitis;
+        	 cout << "sayi : " << ras << "'de sakliydi" << endl;
+            goto bitis;
+		}
+            
+        else
+        {
+        	 cout << "sayi : " << ras << "'de sakliydi" << endl;
+        	 bilemedin+=1;
+        	 
+   			 cout << "bilemedin : "<<bilemedin<<endl;
+   			sayilar[sayigir]=0;
+     	sayilar[ras]=0;
+			  if(bilemedin==5)
+        	goto bitis;
+        	else
+				goto basadon;
+		}
+    }
+bitis:
+
+	cout<<endl;
+	cout<<"oyun bitti"<<endl;
+
+    return 0;
+}
